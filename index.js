373264a70b3d747f3f1125f72bff9277f390ce80
WilDev <wilfridorafaellawson@gmail.com>
2026-03-23 01:26:21 -0500

    prueba back subir

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+// 👇 IMPORTANTE
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Backend funcionando puto");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
