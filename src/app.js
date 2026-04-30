2c19853bb553d8d4d521bcaf6a2c60b6199f85fa
WilDev <wilfridorafaellawson@gmail.com>
2026-04-30 09:28:02 -0500

    infraestructura basica

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Backend funcionando");
+  res.send("Backend funcionando ando");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
