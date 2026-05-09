c57a905cae2fba55fe9550f831b223e0b2af52a1
WilDev <wilfridorafaellawson@gmail.com>
2026-05-09 14:29:46 -0500

    add perfil

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 // configuración de middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
